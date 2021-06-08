e9b22d6c99c9885d09738fe542f34692ecc1a2b6
fadyyessam11 <63158108+fadyyessam11@users.noreply.github.com>
2021-06-08 21:07:13 +0200

    Add files via upload

---
+++ Owner.java
@@ -0,0 +1,60 @@
+
+public class Owner extends User {
+
+    private Playground Playground1;
+
+    /**
+     * default constructor
+     */
+    public Owner() {
+        name = "";
+        email = "";
+        id = 0;
+        password = "";
+        phone = 0;
+        Ewallat = 0;
+
+    }
+
+    /**
+     * param constructor
+     *
+     * @param Playground1
+     */
+    public Owner(Playground Playground1) {
+        this.Playground1 = Playground1;
+    }
+
+    /**
+     * to add owner
+     *
+     * @param playground1
+     * @param name
+     * @param email
+     * @param id
+     * @param password
+     * @param phone
+     * @param Ewallat
+     */
+    public void add_Owner(Playground playground1, String name, String email, int id, String password, long phone, int Ewallat) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.password = password;
+        this.phone = phone;
+        this.Ewallat = Ewallat;
+        this.Playground1 = Playground1;
+    }
+
+    /**
+     * to display info of owner
+     *
+     * @return
+     */
+    @Override
+    public String display() {
+        return "Owner{" + "name=" + name + ", email=" + email + ", id=" + id + ", password=" + password + ", phone=" + phone + ", Ewallat=" + Ewallat + '}';
+
+    }
+
+}
